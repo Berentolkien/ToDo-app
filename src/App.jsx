22a3d696509186c506cfc1a85a64bb7e84393195
Berentolkien <bernalb2014@gmail.com>
2024-10-15 11:47:59 -0300

    as

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home";
 import ToDoApp from "./Pages/To-DoApp";
+import AboutPage from './Pages/AboutPage';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todo" element={<ToDoApp />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
   );
